f4d4ea5be5bde86ecdc8edd7fdc78269d81e96a7
Taxiria <Yggdrasils@users.noreply.github.com>
2021-11-21 19:40:02 +0800

    Update market.py

--- market.py
+++ market.py
@@ -7,7 +7,7 @@ class market(object):
     def __init__(self):
         self.market_api_url = "https://market-api.radiocaca.com/nft-sales/"
         self.market_url = "https://market.radiocaca.com/#/market-place/"
-        self.nft_category = {"Potion" : "15", "Diamond" : "16", "Egg" : "17"}
+        self.nft_category = {"MSL":"7", "MPB":"10", "Potion":"15", "Diamond":"16", "Egg":"17", "Kiss":"20"}
         self.payload = {"pageNo": "1", "pageSize": 10, "sortBy": "single_price", "order": "asc", "name": "", "saleType": "", "category": "", "tokenType": ""}
 
     def get_1155_nft(self, name, number=20):
@@ -19,6 +19,15 @@ class market(object):
             s = name + ": x" + str(nft["count"]).ljust(4) + "  unit price: " + str(int(nft["fixed_price"])//nft["count"]) + "  total price: " + str(nft["fixed_price"]).ljust(9) + "  url: " + self.market_url + str(nft["id"])
             print(s)
 
+    def get_721_nft(self, name, number=20):
+        self.payload["category"] = self.nft_category[name]
+        self.payload["pageSize"] = number
+        self.r = json.loads(requests.get(self.market_api_url, params=self.payload).text)
+        self.nft_list = self.r["list"]
+        for nft in self.nft_list:
+            s = str(nft["token_id"]).ljust(7) + " " + name + "  price: " + str(nft["fixed_price"]).ljust(9) + "  url: " + self.market_url + str(nft["id"])
+            print(s)
+
     def get_metamon(self, number=20):
         self.payload["category"] = "13"
         self.payload["pageSize"] = number
@@ -28,8 +37,8 @@ class market(object):
         for url in self.urls:
             res = json.loads(requests.get(url).text)
             time.sleep(0.5)
-            data = res["data"]
-            properties = data["properties"]
+            nft = res["data"]
+            properties = nft["properties"]
             for i in properties:
                 if i["key"] == "Rarity":
                     rarity = i["value"]
@@ -37,10 +46,11 @@ class market(object):
                     score = i["value"]
                 if i["key"] == "Level":
                     level = i["value"]
-            s = str(data["token_id"]) + " Metamon: " + rarity + "  level: " + level + "  score: " + score + "  price: " + str(data["total_price"]).ljust(7) + "  url: " + self.market_url + str(data["id"])
+            s = str(nft["token_id"]) + " Metamon: " + rarity + "  level: " + level + "  score: " + score + "  price: " + str(nft["fixed_price"]).ljust(9) + "  url: " + self.market_url + str(nft["id"])
             print(s)
 
 if __name__ == "__main__":
     my_market = market()
-    my_market.get_1155_nft("Diamond", 20)  # name = ["Potion", "Diamond", "Egg"]
-    my_market.get_metamon(10)
+    my_market.get_1155_nft("Diamond", 5)  # name = ["Potion", "Diamond", "Egg"]
+    my_market.get_721_nft("Kiss", 5)       # name = ["MPB", "MSL", "Kiss"]
+    my_market.get_metamon(5)
