99ea0308ed615c64d72b81b0e1af4b3a22f89fb8
Taxiria <Yggdrasils@users.noreply.github.com>
2021-11-25 11:20:29 +0800

    Update market.py

--- market.py
+++ market.py
@@ -7,8 +7,14 @@ class market(object):
     def __init__(self):
         self.market_api_url = "https://market-api.radiocaca.com/nft-sales/"
         self.market_url = "https://market.radiocaca.com/#/market-place/"
-        self.nft_category = {"MSL":"7", "MPB":"10", "Potion":"15", "Diamond":"16", "Egg":"17", "Kiss":"20"}
+        self.nft_category = {"N":13, "R":23, "SR":24, "SSR":25, "Potion":15, "Diamond":16, "Egg":17, "Kiss":"20", "MML":7, "MPB":10}
         self.payload = {"pageNo": "1", "pageSize": 10, "sortBy": "single_price", "order": "asc", "name": "", "saleType": "", "category": "", "tokenType": ""}
+
+    def price_sort(self, sort=1):
+        if sort == 1:
+            self.payload["order"] = "asc"
+        if sort == -1:
+            self.payload["order"] = "desc"
 
     def get_1155_nft(self, name, number=20):
         self.payload["category"] = self.nft_category[name]
@@ -28,8 +34,8 @@ class market(object):
             s = str(nft["token_id"]).ljust(7) + " " + name + "  price: " + str(nft["fixed_price"]).ljust(9) + "  url: " + self.market_url + str(nft["id"])
             print(s)
 
-    def get_metamon(self, number=20):
-        self.payload["category"] = "13"
+    def get_metamon(self, name, number=20):
+        self.payload["category"] = self.nft_category[name]
         self.payload["pageSize"] = number
         self.r = json.loads(requests.get(self.market_api_url, params=self.payload).text)
         self.ids = [i["id"] for i in self.r["list"]]
@@ -51,6 +57,9 @@ class market(object):
 
 if __name__ == "__main__":
     my_market = market()
-    my_market.get_1155_nft("Diamond", 5)  # name = ["Potion", "Diamond", "Egg"]
-    my_market.get_721_nft("Kiss", 5)       # name = ["MPB", "MSL", "Kiss"]
-    my_market.get_metamon(5)
+    my_market.price_sort = 1    # 1 is ascending order while -1 is descending order
+    my_market.get_1155_nft("Diamond", 10)  # name = ["Potion", "Diamond", "Egg"]
+    my_market.get_1155_nft("Egg", 10)
+    my_market.get_1155_nft("Potion", 10)
+    my_market.get_721_nft("MPB", 10)       # name = ["MPB", "MML", "Kiss"]
+    my_market.get_metamon("R", 5)  # name = ["N", "R", "SR", "SSR"]
